29be6e4b9888379c4c5a108f79657fab4c62473d
HoTaN <id4acc2014@protonmail.com>
2024-12-28 18:37:10 +0330

    feat: :sparkles: I connected the signup form to the signup API
    I updated the `SignupPage` component to connect the signup form with the backend API. The form now captures user input for email and password using React state.
    On form submission, it sends a POST request to `/api/auth/signup` with the provided credentials. If the registration is successful, the user is redirected to the signin page using `next/router`.

--- components/template/SignupPage.js
+++ components/template/SignupPage.js
@@ -1,12 +1,31 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const signUpHandler = async () => {
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") router.push("/signin");
+  };
+
   return (
     <div className="signin-form">
       <h3>Registration Form</h3>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Register</button>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={signUpHandler}>Register</button>
       <div>
         <p>Already have an account?</p>
         <Link href="/signin">Sign in</Link>
@@ -14,4 +33,5 @@ function SignupPage() {
     </div>
   );
 }
+
 export default SignupPage;
