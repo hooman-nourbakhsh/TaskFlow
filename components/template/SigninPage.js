ff98da4a8b2934d88543e3e111699e61c29520a1
HoTaN <id4acc2014@protonmail.com>
2024-12-30 21:56:11 +0330

    feat: :sparkles: I added error notifications to SignupPage and SigninPage
    I updated the `SignupPage` and `SigninPage` components to display error messages using `toast.error`. If an API call fails, the error message from the response (`data.message`) is now shown as a toast notification. This improves user feedback by providing clear information about what went wrong during signup or signin.

--- components/template/SigninPage.js
+++ components/template/SigninPage.js
@@ -2,6 +2,7 @@ import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function SigninPage() {
   const [email, setEmail] = useState("");
@@ -22,20 +23,26 @@ function SigninPage() {
       redirect: false,
     });
 
-    if (!res.error) router.push("/");
+    if (!res.error) {
+      router.push("/");
+      toast.success("Login successful");
+    } else toast.error(res.error);
   };
 
   return (
-    <div className="signin-form">
-      <h3>Login Form</h3>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={loginHandler}>Login</button>
-      <div>
-        <p>Create an account?</p>
-        <Link href="/signup">Sign up</Link>
+    <>
+      <div className="signin-form">
+        <h3>Login Form</h3>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={loginHandler}>Login</button>
+        <div>
+          <p>Create an account?</p>
+          <Link href="/signup">Sign up</Link>
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
 
--- components/template/SignupPage.js
+++ components/template/SignupPage.js
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
@@ -25,19 +26,23 @@ function SignupPage() {
     const data = await res.json();
     console.log(data);
     if (data.status === "success") router.push("/signin");
+    else toast.error(data.message);
   };
 
   return (
-    <div className="signin-form">
-      <h3>Registration Form</h3>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={signUpHandler}>Register</button>
-      <div>
-        <p>Already have an account?</p>
-        <Link href="/signin">Sign in</Link>
+    <>
+      <div className="signin-form">
+        <h3>Registration Form</h3>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={signUpHandler}>Register</button>
+        <div>
+          <p>Already have an account?</p>
+          <Link href="/signin">Sign in</Link>
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
 
